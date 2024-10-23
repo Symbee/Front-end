9e3ecada52d4d448ddf12e174aa9f816190638c7
Vinicius <vinimendesdasilvaa@gmail.com>
2024-10-23 11:16:27 -0300

    Sextion vinny test

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import Section2 from './components/Section2/Section2'
+
+function App() {
+
+  return (
+    <>
+    <Section2/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Section2/Section2.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Section2.css"
+
+function Section2() {
+  return (
+    <section>
+        <div className="imagemMel" >
+            <img src="./img/liquify.png" alt="mel escorrido" />
+        </div>
+        <div className="textoPorque">
+          <h1>Porque Symbee</h1>
+          <p>Assim como em uma colmeia, o Symbee <br/> Desenvolve a mutalidade</p>
+        </div>
+        <div className='imagemMel2'>
+          <img src="./img/liquify2.png" alt="mel escorrido" />
+        </div>
+
+
+
+    </section>
+
+  )
+}
+
+export default Section2
