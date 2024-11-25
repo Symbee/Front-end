0cb6d43d4f17eb56a3db50273931e84bfb671cb3
felipinhobgtom <felipinhobgouveia@gmail.com>
2024-11-24 22:12:39 -0300

    terminei o back eba

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,12 +2,39 @@ import React from "react";
 import Vetor from "./img/vetor.png";
 import "./login.css";
 import { useNavigate } from "react-router-dom";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as yup from "yup";
+import axios from "axios";
 
 const Login = () => {
   const navegar = useNavigate();
 
-  const handleProfileChoice = () => {
-    navegar("/profile-choice");
+  const validationLogin = yup.object().shape({
+    email: yup
+      .string()
+      .email("Não é um e-mail válido")
+      .required("Esse campo é obrigatório."),
+    password: yup
+      .string()
+      .min(8, "A senha deve conter 8 caractéres")
+      .required("Esse é um campo obrigatório"),
+  });
+
+  const handleClickLogin = (values) => {
+    axios
+      .post("http://localhost:3000/login", {
+        email: values.email,
+        password: values.password,
+      })
+      .then((response) => {
+        console.log(response);
+
+        if (response.data.msg == "Logado com sucesso!") {
+          navegar("/profile-choice");
+        } else {
+          alert(response.data.msg); // Mensagem do servidor, como "Usuário já está cadastrado."
+        }
+      });
   };
 
   return (
@@ -17,17 +44,47 @@ const Login = () => {
           <div className="titleInf">
             <h2>Bem-vindo de volta!</h2>
           </div>
-          <div className="outerInf">
-            <input type="email" placeholder="Endereço de Email " />
-            <input type="password" placeholder="Senha" />
-          </div>
-          <div className="checkbox">
-            <input type="checkbox" name="terms" id="remember" />
-            <p>Lembrar dessas informações?</p>
-          </div>
-          <div className="entrar">
-            <button onClick={handleProfileChoice}>Entrar</button>
-          </div>
+          <Formik
+            className="informations"
+            initialValues={{
+              email: "",
+              password: "",
+            }}
+            onSubmit={handleClickLogin}
+            validationSchema={validationLogin}
+          >
+            <Form className="form-login">
+              <div className="input-login">
+                <Field
+                  name="email"
+                  type="email"
+                  className="login-field"
+                  placeholder="Email"
+                />
+                <ErrorMessage
+                  component="span"
+                  name="email"
+                  className="form-error"
+                />
+              </div>
+              <div className="input-login">
+                <Field
+                  name="password"
+                  type="password"
+                  className="login-field"
+                  placeholder="Senha"
+                />
+                <ErrorMessage
+                  component="span"
+                  name="password"
+                  className="form-error"
+                />
+              </div>
+              <button className="button" type="submit">
+                Entrar
+              </button>
+            </Form>
+          </Formik>
         </div>
         <div className="LogoSvg">
           <img src={Vetor} alt="logo" />
@@ -36,5 +93,4 @@ const Login = () => {
     </section>
   );
 };
-
 export default Login;
